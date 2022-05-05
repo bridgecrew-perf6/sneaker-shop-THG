7359f5b7095b1bb3c66ff96e3bebf92ac6ec5e26
thinh.hoang <thinh.hoang@thlgroup.vn>
2022-05-05 14:01:08 +0700

    api deploy

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000',
-  // baseURL: 'https://sneaker-shop-api.herokuapp.com',
+  // baseURL: 'http://localhost:3000',
+  baseURL: 'https://sneaker-shop-api.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
   },
