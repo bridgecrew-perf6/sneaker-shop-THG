57f12fc2bd45ec6ef52378977b61980dfcb07dc9
thinh.hoang <thinh.hoang@thlgroup.vn>
2022-05-05 13:54:25 +0700

    adding deploy api

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,9 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: 'http://localhost:3000',
+  // baseURL: 'https://sneaker-shop-api.herokuapp.com',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
