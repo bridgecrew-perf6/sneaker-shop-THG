4bf3f0e8743331ada69cf8dee1fbaacbd48ae98f
thinh.hoang <thinh.hoang@thlgroup.vn>
2022-05-06 22:26:14 +0700

    change api

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: 'https://sneaker-shop-api.herokuapp.com',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
